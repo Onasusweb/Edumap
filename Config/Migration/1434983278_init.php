58f54148b1ca6296d7ead467439cf7452c453a6d
s-nakajima <nakajimashouhei@gmail.com>
2015-08-05 11:57:42 +0900

    Fixed initial data generation of Migration

--- Config/Migration/1434983278_init.php
+++ Config/Migration/1434983278_init.php
@@ -146,39 +146,6 @@ class Init extends CakeMigration {
 	);
 
 /**
- * recodes
- *
- * @var array $migration
- */
-	public $records = array(
-		'Plugin' => array(
-			array(
-				'language_id' => 2,
-				'key' => 'edumap',
-				'namespace' => 'netcommons/edumap',
-				'name' => 'edumap連携',
-				'type' => 1,
-				'default_action' => 'edumap/view',
-				'default_setting_action' => 'edumap_blocks/index',
-			),
-		),
-
-		'PluginsRole' => array(
-			array(
-				'role_key' => 'room_administrator',
-				'plugin_key' => 'edumap'
-			),
-		),
-
-		'PluginsRoom' => array(
-			array(
-				'room_id' => '1',
-				'plugin_key' => 'edumap'
-			),
-		),
-	);
-
-/**
  * Before migration callback
  *
  * @param string $direction Direction of migration process (up or down)
@@ -195,36 +162,6 @@ class Init extends CakeMigration {
  * @return bool Should process continue
  */
 	public function after($direction) {
-		if ($direction === 'down') {
-			return true;
-		}
-
-		foreach ($this->records as $model => $records) {
-			if (!$this->updateRecords($model, $records)) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-
-/**
- * Update model records
- *
- * @param string $model model name to update
- * @param string $records records to be stored
- * @param string $scope ?
- * @return bool Should process continue
- */
-	public function updateRecords($model, $records, $scope = null) {
-		$Model = $this->generateModel($model);
-		foreach ($records as $record) {
-			$Model->create();
-			if (!$Model->save($record, false)) {
-				return false;
-			}
-		}
-
 		return true;
 	}
 }
